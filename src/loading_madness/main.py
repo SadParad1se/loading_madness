b5d8f8236581e0d5fb26160ef56658a5894b8888
Jiri Raja <jiri.raja@gmail.com>
2024-10-23 09:43:59 +0200

    Created main.py with sources

---
+++ src/loading_madness/main.py
@@ -0,0 +1,6 @@
+# https://stackoverflow.com/questions/22029562/python-how-to-make-simple-animated-loading-while-process-is-running
+# https://stackoverflow.com/questions/2685435/cooler-ascii-spinners
+# https://stackoverflow.com/questions/66615552/display-multi-line-python-console-ascii-animation
+# https://www.w3schools.com/python/gloss_python_escape_characters.asp
+# https://stackoverflow.com/questions/26697462/print-to-previous-line
+# https://stackoverflow.com/questions/11474391/is-there-go-up-line-character-opposite-of-n
